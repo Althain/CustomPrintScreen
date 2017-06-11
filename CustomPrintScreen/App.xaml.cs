e53da8d5072aaf95913ad1da187aae8c43679e33
Althain2 <hajduczek1007@gmail.com>
2017-06-11 21:12:31 +0200

    Update App.xaml.cs

--- CustomPrintScreen/App.xaml.cs
+++ CustomPrintScreen/App.xaml.cs
@@ -1,40 +1,12 @@
-﻿using System;
 using System.Windows;
-using System.Threading;
-using System.Windows.Threading;
 
 namespace CustomPrintScreen
 {
+    /// <summary>
+    /// Logika interakcji dla klasy App.xaml
+    /// </summary>
     public partial class App : Application
     {
-        private MainWindow window;
 
-        public App(KeyboardHook keyboardHook)
-        {
-            if (keyboardHook == null)
-                throw new ArgumentNullException("keyboardHook");
-
-            keyboardHook.KeyCombinationPressed += KeyCombinationPressed;
-        }
-
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
-            window = new MainWindow();
-        }
-
-        void KeyCombinationPressed(object sender, EventArgs e)
-        {
-            Dispatcher?.Invoke(DispatcherPriority.Normal, new ThreadStart(PrintScreenExe));
-        }
-
-        void PrintScreenExe()
-        {
-            Handler.CreateScreens();
-            MainWindow window = (MainWindow)Application.Current.MainWindow;
-            window.DrawApplication();
-            window.Show();
-            window.Activate();
-        }
     }
 }
